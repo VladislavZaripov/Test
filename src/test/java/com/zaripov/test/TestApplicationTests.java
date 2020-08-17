5e732b7fefc7c91d8f9c6b9868497ccba8580fef
VladislavZaripov <zaripovva@mail.ru>
2020-08-17 17:32:25 +0300

    ...

--- src/test/java/com/zaripov/test/TestApplicationTests.java
+++ src/test/java/com/zaripov/test/TestApplicationTests.java
@@ -1,13 +1,23 @@
 package com.zaripov.test;
 
+import com.zaripov.test.service.Converter;
+import com.zaripov.test.service.ConverterImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class TestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Converter converter = new ConverterImpl();
+
+		String actual = "1,3,2";
+		String expected = "2,4,3";
+
+		assertEquals(expected,actual);
 	}
 
 }
